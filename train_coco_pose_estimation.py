7af2a37bd330779ae78f35738b67fbfb26ae2a03
Naoki Kato <naoki.kato@o-09119-mac.local>
2018-01-18 19:10:36 +0900

    fix

--- train_coco_pose_estimation.py
+++ train_coco_pose_estimation.py
@@ -100,8 +100,8 @@ def compute_loss(imgs, pafs_ys, heatmaps_ys, masks_ys, pafs_t, heatmaps_t,
             # modify soft pafs
             paf_norm = (pt_pafs[:, ::2]**2 + pt_pafs[:, 1::2]**2)
             paf_norm_m = -(paf_norm - 1)**2 + 1
-            multiplier = np.where(paf_norm > 1e-3, paf_norm_m/paf_norm, 0)
-            pt_pafs_m = np.repeat(multiplier, 2, axis=1) * pt_pafs
+            multiplier = xp.where(paf_norm > 1e-3, paf_norm_m/paf_norm, 0)
+            pt_pafs_m = xp.repeat(multiplier, 2, axis=1) * pt_pafs
             # modify soft heatmaps
             pt_heatmaps_m = -(pt_heatmaps - 1)**2 + 1
             pt_heatmaps_m[:, -1] = pt_heatmaps[:, -1]**2
