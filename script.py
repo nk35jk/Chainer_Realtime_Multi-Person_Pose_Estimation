e8485a9cc19b9180369d246e6146eaceaa664330
Naoki Kato <naoki.kato@o-09119-mac.local>
2018-02-20 12:48:22 +0900

    add arguments to script.py

--- script.py
+++ script.py
@@ -7,10 +7,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('arch')
     parser.add_argument('result')
+    parser.add_argument('--start', '-s', type=int, default=10000)
+    parser.add_argument('--end', '-e', type=int, default=300000)
+    parser.add_argument('--step', type=int, default=10000)
     parser.add_argument('--gpu', '-g', type=int, default=-1)
     args = parser.parse_args()
 
-    for i in range(1, 30+1):
-         model_path = '{}/model_iter_{}'.format(args.result, i*10000)
+    for i in range(args.start, args.end+1, args.step):
+        print(i)
+         model_path = '{}/model_iter_{}'.format(args.result, i)
          if os.path.exists(model_path):
-             subprocess.call('python evaluate_coco.py {} {} -g{}'.format(args.arch, model_path, args.gpu), shell=True)
+             subprocess.call('python evaluate_coco.py {} {} -g{}'.format(
+                args.arch, model_path, args.gpu), shell=True)
